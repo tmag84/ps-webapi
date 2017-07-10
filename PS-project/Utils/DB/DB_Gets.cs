1e30ff3cb4c4b9757ae5182bb5ad2e39ccbc4e12
tmag84 <28593@alunos.isel.ipl.pt>
2017-07-10 22:29:59 +0100

    fixes

--- PS-project/Utils/DB/DB_Gets.cs
+++ PS-project/Utils/DB/DB_Gets.cs
@@ -125,11 +125,11 @@ namespace PS_project.Utils.DB
             service.n_subscribers = GetTotatServiceSubscribers(con, service.id);
 
             service.service_events = DB_Gets.GetEvents(con, service.id)
-                .OrderBy(ev => ev.event_date)
+                .OrderByDescending(ev => ev.event_date)
                 .ToList();
 
             service.service_notices = DB_Gets.GetNotices(con, service.id)
-                .OrderByDescending(notice => notice.creation_date)
+                .OrderBy(notice => notice.creation_date)
                 .ToList();
 
             service.service_rankings = DB_Gets.GetRankings(con, service.id)
